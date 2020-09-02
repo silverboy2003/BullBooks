d00f1b34e7b1f1d7d2be9b07bd773a3048441738
silverboy2003 <amitsilverman2003@gmail.com>
2020-09-02 08:56:08 +0300

    database

---
+++ BullBooks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BullBooks.Global" Language="C#" %>
---
+++ BullBooks/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Configuration;
+
+namespace BullBooks
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            System.IO.Directory.SetCurrentDirectory(Server.MapPath("~/"));
+
+            string connString = WebConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            BL.Helper.SetDBConnString(connString);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
