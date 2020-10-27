62ab4ccc6273d205ce8af4983c4bc6a3f692ac2a
silverboy2003 <amitsilverman2003@gmail.com>
2020-10-27 16:27:37 +0200

    need to fix webkit background clip text

--- BullBooks/BookPage.aspx
+++ BullBooks/BookPage.aspx
@@ -2,4 +2,29 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Panel ID="BookPanel" runat="server" CssClass="InformationPanel">
+        <asp:Panel runat="server" ID="InformationPanel" CssClass="InformationContainer">
+            <div class="BookInfoText">
+                <asp:TextBox ID="BookName" runat="server"></asp:TextBox>
+                <asp:TextBox ID="AuthorName" runat="server"></asp:TextBox>
+                <asp:TextBox ID="PublisherName" runat="server"></asp:TextBox>
+                <asp:TextBox ID="NumPages" runat="server"></asp:TextBox>
+                <asp:TextBox ID="NumChapters" runat="server"></asp:TextBox>
+                <asp:TextBox ID="ReleaseDate" runat="server"></asp:TextBox>
+                <asp:TextBox ID="ISBN" runat="server"></asp:TextBox>
+                <asp:TextBox ID="Genres" runat="server"></asp:TextBox>
+
+                <asp:TextBox ID="Synopsis" runat="server"></asp:TextBox>
+            </div>
+
+            <div class="CoverAndScore">
+                <asp:Image runat="server" ID="BookImage" CssClass="BookImage"/>
+
+                <asp:Panel CssClass="Rating" runat="server" ID="StarPanel">
+
+                </asp:Panel>
+
+            </div>
+        </asp:Panel>
+    </asp:Panel>
 </asp:Content>
--- BullBooks/BookPage.aspx.cs
+++ BullBooks/BookPage.aspx.cs
@@ -4,13 +4,68 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BL;
 
 namespace BullBooks
 {
     public partial class BookPage : System.Web.UI.Page
     {
+        private Book thisBook;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadBook();
+                CreateBookPage();
+                GenerateStars();
+            }
+        }
+        protected void CreateBookPage()
+        {
+
+        }
+        protected void GenerateStars()//★
+        {
+            double score = 4.75;
+            int starsCreated = 0; //counts how many stars were created using the score, in order to generate the remaining stars gray
+            while (score >= 1)
+            {
+                Label star = new Label();
+                star.Text = "★";
+                star.Style.Add("color", "yellow");
+                StarPanel.Controls.Add(star);
+                score--;
+                starsCreated++;
+            }
+            if (score != 0)
+            {
+                int percent = (int)Math.Floor(score * 100);
+                Label star = new Label();
+                star.Text = "★";
+                star.CssClass = "Star";
+                star.Style.Add("background", $"linear-gradient(to right, yellow {percent}%, gray {percent}%, gray {100 - percent}%);");
+                //need to create star with gradient using the percentage
+                StarPanel.Controls.Add(star);
+                starsCreated++;
+                score = 0;
+            }
+            for (int i = 5; i > starsCreated; i++)
+            {
+                //create gray star
+            }
+        }
+        protected void LoadBook()//gets and sets the current page's object from application 
+        {
+            string idQuery = Request.QueryString["id"];
+            if (idQuery != null)
+            {
+                int id = int.Parse(idQuery);
+                Dictionary<int, Book> allBooks = (Dictionary<int, Book>)Application["Books"];
+                if (allBooks.ContainsKey(id))
+                    thisBook = allBooks[id];
+                else
+                    thisBook = null;
+            }
         }
     }
 }
