c2cf2300c9e241f1b4a979a3142f88f6de71638e
silverboy2003 <amitsilverman2003@gmail.com>
2020-10-29 17:00:46 +0200

    managed to get the stars css working, amen

--- BullBooks/BookPage.aspx.cs
+++ BullBooks/BookPage.aspx.cs
@@ -26,7 +26,7 @@ namespace BullBooks
         }
         protected void GenerateStars()//★
         {
-            double score = 4.75;
+            double score = 3.3; // change this back later
             int starsCreated = 0; //counts how many stars were created using the score, in order to generate the remaining stars gray
             while (score >= 1)
             {
@@ -41,17 +41,24 @@ namespace BullBooks
             {
                 int percent = (int)Math.Floor(score * 100);
                 Label star = new Label();
-                star.Text = "★";
                 star.CssClass = "Star";
-                star.Style.Add("background", $"linear-gradient(to right, yellow {percent}%, gray {percent}%, gray {100 - percent}%);");
+                star.Text = "★";
+                star.Style.Add("background", $"linear-gradient(to right, yellow {percent}%, gray {percent}%, gray {100 - percent}%)");
+                star.Style.Add("-webkit-text-fill-color", "transparent");
+                star.Style.Add("background-clip", "text");
+                star.Style.Add("-webkit-background-clip", "text");
                 //need to create star with gradient using the percentage
                 StarPanel.Controls.Add(star);
                 starsCreated++;
                 score = 0;
             }
-            for (int i = 5; i > starsCreated; i++)
+            for (int i = 5; i > starsCreated; i--)
             {
-                //create gray star
+                Label star = new Label();
+                star.Text = "★";
+                star.Style.Add("color", "gray");
+                StarPanel.Controls.Add(star);
+                starsCreated++;
             }
         }
         protected void LoadBook()//gets and sets the current page's object from application 
